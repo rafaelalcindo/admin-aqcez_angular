fa712d47b976d4eb35252442254e8fa2c4b6ef18
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-03-16 14:43:00 -0300

    novas features

--- src/app/home/noticias/noticiacada/noticiacada.component.ts
+++ src/app/home/noticias/noticiacada/noticiacada.component.ts
@@ -12,24 +12,32 @@ import { ActivatedRoute,Router } from '@angular/router';
 export class NoticiacadaComponent implements OnInit {
 
   public noticiaIndividual: PrimeirasNoticias;
+  public telaCorreta: boolean = true;
 
   constructor(private noticiasService: NoticiasService,
               private route: ActivatedRoute,
               private router: Router) { }
 
   ngOnInit() {
+
     this.route.params.subscribe((parametro: any) => {
       //console.log(parametro.id);
       this.noticiasService.getNoticiaIndividual(parametro.id)
         .then((noticiaIndividual: PrimeirasNoticias) => {
-          console.log(noticiaIndividual)        
-          if(noticiaIndividual.length <= 0){
+          //console.log(noticiaIndividual);
+          //console.log(Object.keys(noticiaIndividual).length)
+          if (Object.keys(noticiaIndividual).length > 0) {
             //this.router.navigate(['/user', 5]);
-          }else{
-            
-          }     
+            this.telaCorreta = true;
+            this.noticiaIndividual = noticiaIndividual;
+            console.log('Tem dados');
+          } else {
+            this.telaCorreta = false;
+            console.log('Não tem dados');
+          }
         });
     });
+
   }
 
 }
